c9d5253343e2b38f59e0d9994c2df8cc3eb9aa40
David Brown <ndbrown6@users.noreply.github.com>
2019-08-10 17:01:39 -0400

    Update make_id_x.R

--- R/make_id_x.R
+++ R/make_id_x.R
@@ -1,8 +1,8 @@
 'make_id_x' <- function (patient_id,
-						 chrom,
-						 pos,
-						 ref,
-						 alt)
+			 chrom,
+			 pos,
+			 ref,
+			 alt)
 {
 	return(invisible(paste0(patient_id, ":", chrom, ":", pos, ":", ref, ">", alt)))
 }
