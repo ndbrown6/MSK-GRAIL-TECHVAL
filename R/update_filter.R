1c960bc5dc2d33f1f598b85da199e75a5a3a5f72
David Brown <ndbrown6@users.noreply.github.com>
2019-08-13 15:17:48 -0400

    Update update_filter.R

--- R/update_filter.R
+++ R/update_filter.R
@@ -11,23 +11,23 @@
 	gdna = which(pgtkxgdna < min_p)
 	edge = which(is_edge)
 	pass = high_qual %>%
-    	   setdiff(gdna) %>%
-    	   setdiff(edge)
-    low_qual_string = sprintf("QUAL_LT_%d", min_qual)
-    gdna_strings = sprintf("PGTKXGDNA_LT_%0.2f", min_p)
-    edge_string = "IS_EDGE"
+	       setdiff(gdna) %>%
+	       setdiff(edge)
+	low_qual_string = sprintf("QUAL_LT_%d", min_qual)
+	gdna_strings = sprintf("PGTKXGDNA_LT_%0.2f", min_p)
+	edge_string = "IS_EDGE"
   
 	ids = data_frame(i = seq_along(filter))
 	filters = bind_rows(
-				data_frame(i = pass, filter = "PASS"),
-				data_frame(i = low_qual, filter = low_qual_string),
-				data_frame(i = gdna, filter = gdna_strings[gdna]),
-				data_frame(i = edge, filter = edge_string))
+			data_frame(i = pass, filter = "PASS"),
+			data_frame(i = low_qual, filter = low_qual_string),
+			data_frame(i = gdna, filter = gdna_strings[gdna]),
+			data_frame(i = edge, filter = edge_string))
 	filters = filters %>%
-			  group_by(i) %>%
-			  summarize(filter = str_c(filter, collapse = sep)) %>%
-			  ungroup() %>%
-			  full_join(ids) %>%
-			  arrange(i)
+		  group_by(i) %>%
+		  summarize(filter = str_c(filter, collapse = sep)) %>%
+		  ungroup() %>%
+		  full_join(ids) %>%
+		  arrange(i)
 	return(invisible(filters$filter[filters$i]))
 }
