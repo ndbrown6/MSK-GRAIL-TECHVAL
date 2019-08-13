61b202b48ff1892cf000d06228749bedd9aaac26
David Brown <ndbrown6@users.noreply.github.com>
2019-08-13 15:14:50 -0400

    Update log10_axis.R

--- R/log10_axis.R
+++ R/log10_axis.R
@@ -5,6 +5,6 @@
     minor = NULL
     for (j in 2:length(at)) {
     	minor = c(minor, seq(from=at[j-1], to=at[j], length=10))
-	}
-	axis(side=side, at=minor, labels=NA, tcl=par("tcl")*0.65, ...)
+    }
+    axis(side=side, at=minor, labels=NA, tcl=par("tcl")*0.65, ...)
 }
