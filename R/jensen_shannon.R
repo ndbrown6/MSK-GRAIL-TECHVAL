9583f316da1f41e587de0c7a4eea04d96fa6f123
David Brown <ndbrown6@users.noreply.github.com>
2019-08-13 20:03:29 -0400

    Update and rename JS.R to jensen_shannon.R

--- R/jensen_shannon.R
+++ R/jensen_shannon.R
@@ -1,4 +1,4 @@
-'JS' <- function(proportion_vals)
+'jensen_shannon' <- function(proportion_vals)
 {
   return(invisible(compute_entropy(apply(proportion_vals,2,mean))-mean(apply(proportion_vals,1,compute_entropy))))
 }
